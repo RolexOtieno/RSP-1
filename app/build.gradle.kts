7abed7e2c3a43d7588c55c2408632ce651fe2ee8
Rolex Otieno <rolex.otieno@pesapal.com>
2025-06-26 10:56:32 +0300

    RSP-1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = VERSION_11
+        targetCompatibility = VERSION_11
     }
     kotlinOptions {
         jvmTarget = "11"
@@ -38,6 +38,10 @@ android {
         viewBinding = true
     }
     buildToolsVersion = "35.0.0"
+    dependenciesInfo {
+        includeInApk = true
+        includeInBundle = true
+    }
 }
 
 dependencies {
