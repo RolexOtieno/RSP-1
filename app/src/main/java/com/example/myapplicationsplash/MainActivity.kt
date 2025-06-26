5a3b59489ce1d100f0b1983b2f08e4a3da5dc575
Rolex Otieno <rolex.otieno@pesapal.com>
2025-06-26 10:41:12 +0300

    RSP-1

--- app/src/main/java/com/example/myapplicationsplash/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationsplash/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.myapplicationsplash
 
 import android.os.Bundle
-import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
